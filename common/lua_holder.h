10fa86f266a5b40e287b6f75949cd6a5653d6d47
UtopicTahu <147944616+UtopicTahu@users.noreply.github.com>
2024-07-16 13:40:09 -0400

    Update lua_holder.h

--- common/lua_holder.h
+++ common/lua_holder.h
@@ -1,15 +1,7 @@
 //Lua 
-#ifdef RELEASE
 extern "C"
 {
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
 }
-#endif
-
-#ifdef DEBUG
-#include "lua.h"
-#include "lauxlib.h"
-#include "lualib.h"
-#endif // DEBUG
